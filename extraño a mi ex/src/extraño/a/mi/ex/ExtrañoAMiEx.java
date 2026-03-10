347fc9ff135bf2f1d79c56fbaa3f9115d6f1fe92
pepe10xd <brianandres20108@gmail.com>
2026-03-10 16:08:40 -0300

    ejercicio variables

---
+++ extraño a mi ex/src/extraño/a/mi/ex/ExtrañoAMiEx.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package extraño.a.mi.ex;
+
+/**
+ *
+ * @author ET20
+ */
+public class ExtrañoAMiEx {
+
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+int  q = 3;
+int f = 5;
+float x = 230;
+float y = 100;
+float z = 34;
+char a = 'A';
+char b = 'B';
+char c = 'C';
+double raiz1 = 6.33;
+double raiz2 = 8.2222;
+long contador = 9000000;
+short indicador = 1000;
+double precio = 300.67;
+double precioFinal = 450.67;
+char car1 = 'K';
+char car2 = 'G';
+byte valor = 104;
+boolean primero = true;
+boolean ultimo = false;
+String nombre = "tomas";
+
+
+
+       System.out.println(q);
+       System.out.println(f);
+       System.out.println(x);
+       System.out.println(z);
+       System.out.println(y);
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(c);
+       System.out.println(raiz1);
+       System.out.println(raiz2);
+       System.out.println(contador);
+       System.out.println(indicador);
+       System.out.println(precio);
+       System.out.println(precioFinal);
+       System.out.println(car1);
+       System.out.println(car2);
+       System.out.println(valor);
+       System.out.println(primero);
+       System.out.println(ultimo);
+       System.out.println(nombre);
+    }
